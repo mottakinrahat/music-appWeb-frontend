3389a5cd0e51d1e7b92f971059269dccd64e4882
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2024-08-22 16:00:55 +0600

    type updated on music page.tsx

--- src/app/(withCommonLayout)/music/page.tsx
+++ src/app/(withCommonLayout)/music/page.tsx
@@ -58,11 +58,11 @@ const AudioList: React.FC = () => {
     <div className="bg-[#F7F7F7] h-screen mx-[120px] mt-10">
       <h2 className="text-3xl font-bold">New release</h2>
       <p className="text-[16px] max-w-[588px]">
-        Get your ears on the hottest new tracks, from chart-topping anthems to
-        underground gems bubbling up from the scene.
+        Get your ears on the hottest new tracks, from chart-topping anthems to underground gems bubbling up from the
+        scene.
       </p>
       <div className="flex flex-wrap justify-between items-center mt-[48px]">
-        {tracks.map((track) => (
+        {tracks.map((track: any) => (
           <Link key={track?.id} href={`player/${track?.id}`}>
             <div className="max-w-[282px]">
               <Image
