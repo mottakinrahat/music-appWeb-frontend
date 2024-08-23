9fa2791afbb7edcb92426b2bedf661c45bce5356
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-23 15:43:05 +0600

    name swaped with player and music

--- src/app/(withCommonLayout)/music/page.tsx
+++ src/app/(withCommonLayout)/music/page.tsx
@@ -3,67 +3,24 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-// Define the Track interface
-// export interface Track {
-//   id?: number;
-//   title?: string;
-//   artist?: string;
-//   album?: string;
-//   artwork: string;
-//   url?: string;
-// }
-
-// // Define the tracks array with the Track type
-// export const tracks: Track[] = [
-//   {
-//     id: 1,
-//     title: "Enna Sona",
-//     artist: "Arijit Singh",
-//     album: "Ok Janu",
-//     artwork:
-//       "https://resize.indiatvnews.com/en/centered/newbucket/1200_675/2016/12/ok-jaanu-1481374642.jpg",
-//     url: "http://hindisongs.fusionbd.com/downloads/mp3/hindi/Single_Tracks/Enna_Sona-Ok_Jaanu_FusionBD.Com.mp3",
-//   },
-//   {
-//     id: 2,
-//     title: "Tujhe Kitna Chahne Lage hum",
-//     artist: "Arijit Singh",
-//     album: "Kabir Singh",
-//     artwork:
-//       "https://static.abplive.com/wp-content/uploads/2019/04/06192544/kabir-singh.jpg?impolicy=abp_cdn&imwidth=1200&height=675",
-//     url: "http://hindisongs.fusionbd.com/downloads/mp3/hindi/Kabir_Singh/Tujhe_Kitna_Chahne_Lage-Kabir_Singh_FusionBD.Com.mp3",
-//   },
-//   {
-//     id: 3,
-//     title: "Kaise Hua",
-//     artist: "Vishal Mishra",
-//     album: "Kabir Singh",
-//     artwork:
-//       "https://static.abplive.com/wp-content/uploads/2019/04/06192544/kabir-singh.jpg?impolicy=abp_cdn&imwidth=1200&height=675",
-//     url: "https://soundcloud.com/khadija-malick-416298659/kaise-hua-kabir-singh?si=bd2186d41c47487f8204ae1731f99deb&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
-//   },
-//   // Add more tracks as needed
-// ];
-
-const AudioList: React.FC = () => {
-  const [tracks, setTraks] = useState<any>([]);
+const AudioList = () => {
+  const [tracks, setTraks] = useState([]);
   useEffect(() => {
     fetch("/tracks.json")
       .then((data) => data.json())
       .then((tracks) => setTraks(tracks));
   }, []);
 
-  console.log(tracks);
   return (
-    <div className=" h-screen mx-[120px] mt-10">
+    <div className="bg-[#F7F7F7] h-screen mx-[120px] mt-10">
       <h2 className="text-3xl font-bold">New release</h2>
       <p className="text-[16px] max-w-[588px]">
-        Get your ears on the hottest new tracks, from chart-topping anthems to underground gems bubbling up from the
-        scene.
+        Get your ears on the hottest new tracks, from chart-topping anthems to
+        underground gems bubbling up from the scene.
       </p>
       <div className="flex flex-wrap justify-between items-center mt-[48px]">
-        {tracks.map((track: any) => (
-          <Link key={track?.id} href={`player/${track?.id}`}>
+        {tracks?.map((track: any) => (
+          <Link key={track?.id} href={`music/${track?.id}`}>
             <div className="max-w-[282px]">
               <Image
                 src={track?.artwork}
@@ -72,7 +29,7 @@ const AudioList: React.FC = () => {
                 className="h-[282px] w-[282px] object-cover object-center rounded-xl"
                 alt={track?.title || "Track Artwork"}
               />
-              <h2 className="text-[20px] font-bold">{track?.title}</h2>
+              <h2 className="text-[24px] font-bold">{track?.title}</h2>
               <h5 className="text-[16px]">{track?.artist}</h5>
             </div>
           </Link>
