7cda758da2fe94b8e0ac7ed3bb3738cb7ec6003c
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2024-08-24 16:02:10 +0600

    home page button color changed

--- src/app/(withCommonLayout)/signup/component/SignupPage.tsx
+++ src/app/(withCommonLayout)/signup/component/SignupPage.tsx
@@ -4,9 +4,9 @@ import React from "react";
 
 const SignupPage: React.FC = () => {
   return <div className="min-h-screen flex items-center justify-center">
-    <DForm>
+    {/* <DForm>
         <h1>Sign Up</h1>
-    </DForm>
+    </DForm> */}
   </div>;
 };
 
