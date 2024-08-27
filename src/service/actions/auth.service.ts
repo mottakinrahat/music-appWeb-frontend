e9a3367234f8e345b5c47bb74d7a1cfa3072d1cd
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2024-08-27 09:36:39 +0600

    create account page added and show hide password functionality added

--- src/service/actions/auth.service.ts
+++ src/service/actions/auth.service.ts
@@ -1,39 +1,35 @@
-
-
-
-import { authKey } from '@/constants/authkey';
-import { decodedToken } from '@/utils/jwt';
-import { getFromLocalStorage, removeFromLocalStorage, setToLocalStorage } from '@/utils/local-storage';
-
+import { authKey } from "@/constants/authkey";
+import { decodedToken } from "@/utils/jwt";
+import { getFromLocalStorage, removeFromLocalStorage, setToLocalStorage } from "@/utils/local-storage";
 
 export const storeUserInfo = ({ token }: { token: string }) => {
-   //   console.log(token);
-   return setToLocalStorage(authKey, token);
+  //   console.log(token);
+  return setToLocalStorage(authKey, token);
 };
 
 export const getUserInfo = () => {
-   const authToken = getFromLocalStorage(authKey);
-   //   console.log(authToken);
-   if (authToken) {
-      const decodedData: any = decodedToken(authToken);
-      return {
-         ...decodedData,
-         role: decodedData?.role?.toLowerCase(),
-      };
-   } else {
-      return '';
-   }
+  const authToken = getFromLocalStorage(authKey);
+  //   console.log(authToken);
+  if (authToken) {
+    const decodedData: any = decodedToken(authToken);
+    return {
+      ...decodedData,
+      role: decodedData?.role?.toLowerCase(),
+    };
+  } else {
+    return "";
+  }
 };
 
 export const isLoggedIn = () => {
-   const authToken = getFromLocalStorage(authKey);
-   if (authToken) {
-      return !!authToken;
-   }
+  const authToken = getFromLocalStorage(authKey);
+  if (authToken) {
+    return !!authToken;
+  }
 };
 
 export const removeUser = () => {
-   return removeFromLocalStorage(authKey);
+  return removeFromLocalStorage(authKey);
 };
 
 // export const getNewAccessToken = async () => {
