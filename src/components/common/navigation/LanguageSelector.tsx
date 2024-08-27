95caa14aec26811ea70edd551b7a7b5cc7dbbdd0
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-27 08:30:27 +0600

    added new language selector in components

---
+++ src/components/common/navigation/LanguageSelector.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Image from "next/image";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import usFlag from "@/assets/flags/united-states.png";
+import ukFlag from "@/assets/flags/united-kingdom.png";
+import ausFlag from "@/assets/flags/australia.png";
+
+const LanguageSelector = () => {
+  return (
+    <Select defaultValue="us">
+      <SelectTrigger className="w-[180px] focus-visible:shadow-none focus-visible:outline-0 focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:border-accent">
+        <SelectValue placeholder="Select a country" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectItem value="us">
+            <Image
+              src={usFlag}
+              alt="US"
+              width={16} // Setting width to 16px
+              height={16} // Setting height to 16px
+              className="inline-block mr-2"
+            />
+            US
+          </SelectItem>
+          <SelectItem value="uk">
+            <Image
+              src={ukFlag}
+              alt="UK"
+              width={16}
+              height={16}
+              className="inline-block mr-2"
+            />
+            UK
+          </SelectItem>
+          <SelectItem value="aus">
+            <Image
+              src={ausFlag}
+              alt="AUS"
+              width={16}
+              height={16}
+              className="inline-block mr-2"
+            />
+            AUS
+          </SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default LanguageSelector;
--- src/components/common/navigation/LogInSignUpNav.tsx
+++ src/components/common/navigation/LogInSignUpNav.tsx
@@ -18,6 +18,7 @@ import {
 import usFlag from "@/assets/flags/united-states.png";
 import ukFlag from "@/assets/flags/united-kingdom.png";
 import ausFlag from "@/assets/flags/australia.png";
+import LanguageSelector from "./LanguageSelector";
 
 const navSchema = z.object({});
 
@@ -36,33 +37,7 @@ const LogInSignUpNav: React.FC = () => {
           <DForm onSubmit={handleSubmit} resolver={navSchema}>
             {/* Dropdown Country */}
             <div>
-              <Select defaultValue="us">
-                <SelectTrigger className="w-[180px] focus-visible:shadow-none focus-visible:outline-0 focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:border-accent">
-                  <SelectValue placeholder="Select a country" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectItem value="us">
-                      <Image
-                        src={usFlag}
-                        alt="US"
-                        width={16} // Setting width to 16px
-                        height={16} // Setting height to 16px
-                        className="inline-block mr-2"
-                      />
-                      US
-                    </SelectItem>
-                    <SelectItem value="uk">
-                      <Image src={ukFlag} alt="UK" width={16} height={16} className="inline-block mr-2" />
-                      UK
-                    </SelectItem>
-                    <SelectItem value="aus">
-                      <Image src={ausFlag} alt="AUS" width={16} height={16} className="inline-block mr-2" />
-                      AUS
-                    </SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
+              <LanguageSelector />
             </div>
           </DForm>
         </div>
